f86b88f6423da028af80eef7192a642bbcd19c9b
DefenestratedBirb. <lanchuzheng@gmail.com>
2025-01-23 21:40:40 +1100

    Thursday 23rd of Jan 2025 Update.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('.dropdown > a').forEach(item => {
-    item.addEventListener('click', event => {
-        event.preventDefault(); // Prevent default link behaviour
-        const dropdownMenu = item.nextElementSibling;
-        dropdownMenu.style.display = 
-            dropdownMenu.style.display === 'block' ? 'none' : 'block';
-    });
+document.querySelectorAll(".dropdown > a").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.preventDefault();
+    const dropdownMenu = item.nextElementSibling;
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+  });
 });
